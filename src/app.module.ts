1dde4b739b9a658377eba079c7aa0d31b1ea86fb
sai-gandhi <2200032934@kluniversity.in>
2026-06-10 12:36:36 +0530

    fix: use DATABASE_URL for Neon DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
-TypeOrmModule.forRootAsync({
-  inject: [ConfigService],
-  useFactory: (config: ConfigService) => ({
-    type: 'postgres',
-    url: config.get('DATABASE_URL'),
-    entities: [User, DoctorProfile, PatientProfile],
-    synchronize: false,
-    migrations: ['dist/database/migrations/*.js'],
-    ssl: { rejectUnauthorized: false },
-  }),
-}),
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { DoctorModule } from './doctor/doctor.module';
+import { PatientModule } from './patient/patient.module';
+import { User } from './users/user.entity';
+import { DoctorProfile } from './doctor/doctor.entity';
+import { PatientProfile } from './patient/patient.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get('DATABASE_URL'),
+        entities: [User, DoctorProfile, PatientProfile],
+        synchronize: false,
+        ssl: { rejectUnauthorized: false },
+      }),
+    }),
+    AuthModule,
+    UsersModule,
+    DoctorModule,
+    PatientModule,
+  ],
+})
+export class AppModule {}
